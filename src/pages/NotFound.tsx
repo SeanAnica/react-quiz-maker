242275604a62a85a190b8d505dd4b0504b8902de
ANICA Sean <sean.anica@soprasteria.com>
2025-03-17 11:18:26 +0100

    fix: adaptation to gh pages for 404 error page and replace <a> by react router <Link...>

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router';
+
 export type NotFoundProps = {
   message?: string;
   title?: string;
@@ -11,12 +13,12 @@ const NotFound = ({
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       <h1 className="text-6xl font-bold text-red-500">{title}</h1>
       <p className="text-xl mt-4">{message}</p>
-      <a
-        href="/"
+      <Link
+        to="/"
         className="mt-6 px-6 py-3 bg-blue-600 hover:bg-blue-700 text-white font-semibold rounded-lg transition duration-300"
       >
         Back to quiz creation
-      </a>
+      </Link>
     </div>
   );
 };
